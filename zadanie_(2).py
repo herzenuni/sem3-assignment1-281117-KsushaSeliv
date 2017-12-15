48003c5cf33008ffc0da654f59c309a060704e20
KsushaSeliv <KsenijaSeliv@yandex.ru>
2017-12-15 11:26:47 +0300

    Update zadanie_(2).py

--- zadanie_(2).py
+++ zadanie_(2).py
@@ -1,1 +1,15 @@
+#Задание 2
 
+def function_1(chislo):
+  x=str(chislo)
+  summa=0
+  for i in x:
+    summa += int(i)**2
+  return summa
+  
+def function_2():
+  for i in range(10, 1000):
+    if function_1(i) % 17 == 0:
+      print('Числа, сумма цифр которых в квадрате делится на 17: {}' .format(i))
+
+function_2()  
